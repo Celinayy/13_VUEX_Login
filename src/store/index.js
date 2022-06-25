0254a469e0edc9f959f2c68f98c9d258d76c93d9
Vangel Celina Hanna <vangelcelina9@gmail.com>
2022-06-25 12:39:15 +0200

    bugfix

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,24 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-    state:{},
+    // eltároljuk a user adatait
+    state:{
+        user:{
+            email:'',
+            username:'',
+            status: false,
+        },
+    },
+    // lekérdező függvények a változókból
     getters:{},
-    mutations:{},
+    // függvényt írunk bele
+    mutations:{
+        setStatus(state, loggedInUser){
+            state.user.email = loggedInUser.email
+            state.user.username = loggedInUser.username
+            state.user.status = loggedInUser.status
+        }
+    },
     actions:{},
     modules:{}
 })
---
+++ src/views/LogoutView.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Sikeres kijelentkezés!</h1>
+</template>
+
+<script>
+import store from '@/store'
+
+export default {
+    name:'logout-view',
+    created(){
+      let loggedInUser = {
+        email: "",
+        username: "",
+        status: false,
+      };
+      store.commit("setStatus", loggedInUser);
+
+    },
+};
+</script>
+
+<style>
+
+</style>
